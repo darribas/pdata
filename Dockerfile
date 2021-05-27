86433b8fe66caa1e54711aeeab8bff70e5bf2e90
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2021-05-27 17:43:39 +0100

    Add more to GH lib

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
 FROM datasetteproject/datasette:0.54
 
+RUN apt-get update -qq \
+ && apt-get install -y --no-install-recommends git \
+ && apt-get autoclean \
+ && apt-get autoremove \
+ && apt-get clean
+
 RUN pip install \
           datasette-leaflet-geojson \
           datasette-render-timestamps \
--- gh/archive.py
+++ gh/archive.py
@@ -2,30 +2,61 @@
 Create and update an archive of Github repositories
 """
 
+import os
+import git
 import click
+import subprocess
+import sqlite_utils
 
-def get_repo_list(db):
+def get_repo_list(db_path):
     """
     Extract list of git URLs for repos
+    
+    Arguments
+    ---------
+    db_path : str
+              Path to sqlite DB with repo information
+    Returns
+    -------
+    repo_list : list
+                List of strings containing git URL for each repo
     """
+    db = sqlite_utils.Database(db_path)
+    repo_list = []
+    for row in db.table("repos").rows:
+        repo_list.append(row["html_url"]+".git")
     return repo_list
 
 def clone_update_repo(git_url, folder_p):
     """
     Check existence of local copy and clone/fetch otherwise
     """
-    return None
+    owner = git_url.split("/")[-2]
+    repo_name = git_url.split("/")[-1].strip(".git")
+    name = os.path.join(owner, repo_name)
+    repo_folder = os.path.join(folder_p, name)
+    # Update
+    if os.path.exists(repo_folder):
+        out = subprocess.run(
+            ["cd", repo_folder, "&&","git", "fetch", "origin"]
+        )
+        log = f"Updated {repo_folder}"
+    # Clone
+    else:
+        out = subprocess.run(
+            ["git", "clone", git_url, repo_folder]
+        )
+        log = f"Cloned {git_url} into {repo_folder}"
+    return log
 
 @click.command()
-@click.option(
-        "-db",
-        "--path-to-db",
-        help = "Path to DB with list of repositories"
+@click.argument(
+        "db_path",
+        required=True
 )
-@click.option(
-        "-f",
-        "--folder-for-backup",
-        help = "Path to the folder where to store locally repositories"
+@click.argument(
+        "folder_p",
+        required=True
 )
 def run_update(db_path, folder_p):
     """
